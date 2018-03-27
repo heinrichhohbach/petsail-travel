05b02def241d6f997271c2a4c895e89070016d56
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-03-27 21:02:39 +0100

    first commit

---
+++ accounts/tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+# Create your tests here.
+class SimpleTest(TestCase):
+    def test_adding_something_simple(self):
+        self.assertEqual(1+2, 3)
+
+    def test_adding_something_isnt_equal(self):
+        self.assertNotEqual(1+2, 4)
+
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Elliott\PycharmProjects\Pet_Sail\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
