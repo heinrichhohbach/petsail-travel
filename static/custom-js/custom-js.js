ac8003269307be059a36ab0c7afd83806b0196db
Heinrich Hohbach <heinrich.hohbach@gmail.com>
2018-04-24 21:55:25 +0100

    JS changes 3

--- static/custom-js/custom-js.js
+++ static/custom-js/custom-js.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
         $("#header-index-id").hover(function(){
-            $(this).removeClass('.header-index').addClass('.header-index2');
-            $(this).removeClass('.header-index2').addClass('header-index');
+            $('#index-row-style').hide();
         });
 });
